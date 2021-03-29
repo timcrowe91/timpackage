c0743fc1e47bae3fd591c878fef76818b33f1b13
tim crowe <tim_crowe@hotmail.com>
2021-03-29 11:53:07 -0600

    Initial push

---
+++ timpackage/lib.py
@@ -0,0 +1,18 @@
+def try_me():
+    while True:
+        try:
+            print("Input first number:")
+            a = float(input())
+            break
+        except ValueError:
+            print("That's not a number, try again")
+    
+    while True:
+        try:
+            print("Input second number:")
+            b = float(input())
+            break
+        except ValueError:
+            print("That's not a number, try again")
+
+    return f"{a} + {b} = {a + b}"
